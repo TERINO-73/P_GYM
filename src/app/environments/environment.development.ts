d45f138a94b3d92352776e838a44f5b22cd69b6e
XXXXX <XXXXX@g.educaand.es>
2025-02-18 11:09:01 +0100

    Cambio Jesus 18/2 11:08

---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    API_URL_ale:"http://localhost/serviciosWeb/Gimnasio/servidor.php",
+    API_URL_jesus:"http://localhost/P_GYM/P_GYM/Gimnasio/servidor.php",
+
+};
--- src/app/servicios/servicios.service.ts
+++ src/app/servicios/servicios.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Clase } from '../modelos/clase';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
 
-  private url:string = "http://localhost/serviciosWeb/Gimnasio/servidor.php";
+  private url = environment.API_URL_jesus;
 
   constructor(private http: HttpClient) { }
 
@@ -15,6 +16,11 @@ export class ServiciosService {
     let pa = JSON.stringify({
       servicio: "listarClases"
     });
+    return this.http.post<Clase[]>(this.url, pa);
+  }
+
+  borrarPersona(id:number){
+    let pa = JSON.stringify({ accion: 'borrarClase', id: id});
     return this.http.post<Clase[]>(this.url, pa);
   }
 }
