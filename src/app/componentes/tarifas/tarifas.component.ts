264e4dc11e33829c14ee80aafc02a9803559c12d
Terino <jesusteri14@gmail.com>
2025-02-23 17:43:41 +0100

    COMMIT

--- src/app/componentes/tarifas/tarifas.component.ts
+++ src/app/componentes/tarifas/tarifas.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-tarifas',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './tarifas.component.html',
-  styleUrl: './tarifas.component.css'
+  styleUrls: ['./tarifas.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter', [
+        animate('500ms ease-in', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class TarifasComponent {
-
+  isVisible = true;
+  tarifas = [
+    { nombre: 'Mensual', precio: '30€', descripcion: 'Acceso ilimitado durante un mes.' },
+    { nombre: 'Trimestral', precio: '80€', descripcion: 'Acceso ilimitado durante tres meses.' },
+    { nombre: 'Anual', precio: '300€', descripcion: 'Acceso ilimitado durante un año.' },
+    { nombre: 'Clase Individual', precio: '10€', descripcion: 'Acceso a una clase individual.' }
+  ];
 }
