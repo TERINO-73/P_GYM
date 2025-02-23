de547765d7856194659035a7778cf2cd2e1aa1c9
Terino <jesusteri14@gmail.com>
2025-02-23 17:01:04 +0100

    JEUS

---
+++ src/app/componentes/horario/horario.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horario',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './horario.component.html',
+  styleUrls: ['./horario.component.css']
+})
+export class HorarioComponent {
+  isVisible = false;
+  diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  horario = [
+    { dia: 'Lunes', hora: '08:00 - 09:00', clase: 'Yoga' },
+    { dia: 'Lunes', hora: '18:00 - 19:00', clase: 'Spinning' },
+    { dia: 'Martes', hora: '09:00 - 10:00', clase: 'Pilates' },
+    { dia: 'Miércoles', hora: '17:00 - 18:00', clase: 'Boxeo' },
+    { dia: 'Jueves', hora: '10:00 - 11:00', clase: 'Crossfit' },
+    { dia: 'Viernes', hora: '19:00 - 20:00', clase: 'Zumba' },
+    { dia: 'Sábado', hora: '11:00 - 12:00', clase: 'Calistenia' },
+    { dia: 'Domingo', hora: '09:00 - 10:00', clase: 'Meditación' }
+  ];
+}
